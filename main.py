1cfd2fedd56f861b059840f930f1f2c5ddb91d62
shiva768 <reika.github@zelretch.com>
2018-07-23 03:40:52 +0900

    exitじゃダメだった

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -9,17 +10,17 @@ async def on_ready():
     print(client.user.id)
     print('------')
     try:
-        user = await client.get_user_info('446237795342745600')
-        channel = await  client.start_private_message(user)
-        # tip_channel = client.get_channel('446587631749562388')
-        # tmp = await client.send_message(tip_channel, './mining 10')
-        hogemi = await client.get_user_info('446234539681906689')
-        hogemi_ch = await  client.start_private_message(hogemi)
+        # user = await client.get_user_info('446237795342745600')
+        # channel = await  client.start_private_message(user)
+        ura_channel = client.get_channel('465840548842831874')
+        tip_channel = client.get_channel('446588000462438402')
+        tmp = await client.send_message(tip_channel, './mining 10')
+        # hogemi = await client.get_user_info('446234539681906689')
+        # hogemi_ch = await  client.start_private_message(hogemi)
         # client.g
-        tmp = await client.send_message(hogemi_ch, './tip random 10')
-        tmp = await client.send_message(channel, './tip random 10')
+        tmp = await client.send_message(ura_channel, './tip random 10')
     finally:
-        client.logout()
-        exit(0)
+        tmp = await client.logout()
+
 
 client.run("")
